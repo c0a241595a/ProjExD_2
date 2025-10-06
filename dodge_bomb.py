2043fb1eb4bfcc6b0237473d0d45179a69ec3448
c0a24159 <c0a241595a@edu.teu.ac.jp>
2025-10-07 06:19:12 +0900

    こうかとんと爆弾判定

---
+++ dodge_bomb.py
@@ -0,0 +1,78 @@
+import os
+import random
+import sys
+import pygame as pg
+
+WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect or ばくだんRect
+    戻り値：判定結果タプル（横方向，縦方向）
+    画面内ならTrue／画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向にはみ出ていたら
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: # 縦方向にはみ出ていたら
+        tate = False
+    return yoko, tate
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空Surface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い爆弾円
+    bb_img.set_colorkey((0, 0, 0))  # 四隅の黒い部分を透過
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
+    vx, vy = +5, +5  # 爆弾の速度
+    clock = pg.time.Clock()
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return
+        screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾の衝突判定
+            return  # ゲームオーバー
+
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  # 横方向の移動量を加算
+                sum_mv[1] += mv[1]  # 縦方向の移動量を加算
+
+        kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  # 爆弾移動
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
+        screen.blit(bb_img, bb_rct)  # 爆弾描画
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
